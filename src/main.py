2845c265432b398aa22b771dd55b74af1cb99342
gaato <g.furuya@gaato.net>
2024-06-24 18:05:01 +0900

    Escape `@someone`

--- src/main.py
+++ src/main.py
@@ -38,12 +38,13 @@ def convert_youtube_url(url: str) -> Optional[str]:
 
     youtube_url_pattern = re.compile(
         r"(https?://)?(www\.|m\.)?youtube\.com/(watch\?v=|live/|shorts/)([^&\s/?]+)|"
-        r"(https?://)?youtu\.be/([^&\s/?]+)"
+        r"(https?://)?youtu\.be/([^&\s/?]+)|"
+        r"(https?://)?holodex\.net/watch/([^&\s/?]+)"
     )
 
     match = youtube_url_pattern.match(url)
     if match:
-        video_id = match.group(4) or match.group(6)
+        video_id = match.group(4) or match.group(6) or match.group(8)
         return youtube_short_url_format.format(video_id)
     else:
         return None
@@ -73,11 +74,10 @@ async def on_message(message: discord.Message):
     )[0]
     thumbnail_url = youtube_data_fetcher.get_thumbnail_url(video_details["id"])
     media_id = twitter_poster.media_upload(thumbnail_url)
+    video_title = re.sub(r"@(\w+)", r"@\u200B\1", video_details["snippet"]["title"])
     try:
         twitter_poster.post_tweet(
-            f"Now I'm watching...\n\n"
-            f"{video_details['snippet']['title']}\n"
-            f"{youtube_url}",
+            f"Now I'm watching...\n\n" f"{video_title}\n" f"{youtube_url}",
             media_ids=[media_id],
         )
     except Exception as e:
